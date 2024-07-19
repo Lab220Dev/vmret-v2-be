c66f045b48de7422f1c6592380ce06313d99a083
Gabriel Ramos <gtorres321@gmail.com>
2024-07-19 14:19:18 -0300

    Update loginController.js
    Alteração no metodo de login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -9,26 +9,35 @@ const opcoes = {
 
 async function login(request, response) {
     try {
-        let query = 'SELECT * FROM Usuarios WHERE 1 = 1';
-
-        if (request.body.email) {
-            query += ` AND email = '${request.body.email}'`;
-        }
-        if (request.body.senha) {
-            let hashMD5 = CryptoJS.MD5(request.body.senha).toString();
-            query += ` AND senha = '${hashMD5}'`;
-            const result = await new sql.Request().query(query);
-            let Usuario = result.recordset;
-            delete Usuario[0].senha;
-            const token = jwt.sign({ Usuario }, segredo, opcoes);
-            response.status(200).json({ token, Usuario });
+        const { email, senha } = request.body;
+        if (!email || !senha) {
+            response.status(400).json("E-mail e senha são obrigatórios");
             return;
         }
-        response.status(401).json("E-mail ou senha inválidos");
+        const hashMD5 = CryptoJS.MD5(senha).toString();
+        const query = `
+        SELECT * FROM Usuarios
+        WHERE email = @Email
+          AND senha = @Senha`;
+        const result = await new sql.Request()
+            .input('Email', sql.VarChar, email)
+            .input('Senha', sql.VarChar, hashMD5)
+            .query(query)
+        const Usuario = result.recordset[0];
+
+        if (Usuario) {
+            delete Usuario.senha;
+            const token = jwt.sign({ Usuario }, segredo, opcoes);
+            response.status(200).json({ token, Usuario });
+        } else {
+            response.status(401).json("E-mail ou senha inválidos");
+        }
+
     } catch (error) {
         console.error('Erro ao executar consulta:', error.message);
         response.status(500).send('Erro ao executar consulta');
     }
+
 }
 
 async function logout(request, response) {
