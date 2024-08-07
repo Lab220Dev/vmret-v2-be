c863e8532068a7b2f3d6a6e4c00e67def01a8b97
Gabriel Ramos <gtorres321@gmail.com>
2024-08-07 09:26:56 -0300

    dist

---
+++ dist/assets/LoadingSpinner-d16fdc18.js
@@ -0,0 +1,1 @@
+import{_ as n,o,c as s,d as e}from"./index-ac32e295.js";const a={},c={class:"loading-overlay"},t=e("div",{class:"loading-container"},[e("div",{class:"loading-spinner"}),e("p",{style:{"margin-top":"15px",color:"#333"}},"Carregando... Aguarde")],-1),r=[t];function _(i,d){return o(),s("div",c,r)}const p=n(a,[["render",_]]);export{p as L};
