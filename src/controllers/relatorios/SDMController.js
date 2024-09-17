bf65435a7d72575143110777baa0da5efa542b77
Gabriel Ramos <gtorres321@gmail.com>
2024-09-17 17:36:17 -0300

    Update SDMController.js

--- src/controllers/relatorios/SDMController.js
+++ src/controllers/relatorios/SDMController.js
@@ -1,9 +1,8 @@
 const sql = require('mssql');
-const { logWithOperation } = require('../../middleware/Logger');
 
 async function relatorio(request, response) {
     try {
-        const { id_cliente, id_dm, dia ,id_usuario} = request.body;
+        const { id_cliente, id_dm, dia } = request.body;
 
         if (!id_cliente) {
             return response.status(401).json("ID do cliente não enviado");
@@ -20,7 +19,7 @@ async function relatorio(request, response) {
             SELECT * FROM DM_status 
             WHERE ID_DM = @id_dm 
               AND id_cliente = @id_cliente 
-              AND CONVERT(date, status_time) = @dia
+              AND CONVERT(date, dataHora) = @dia
         `;
 
         const dbRequest = new sql.Request();
@@ -29,11 +28,9 @@ async function relatorio(request, response) {
         dbRequest.input('dia', sql.Date, formattedDate);
 
         const result = await dbRequest.query(query);
-       // logWithOperation('info', `O usuario ${id_usuario} Gerou um relatorio`, `sucesso`, 'Relatorio Status DM', id_cliente, id_usuario);
         response.status(200).json(result.recordset);
     } catch (error) {
         console.error('Erro ao executar consulta:', error.message);
-       // logWithOperation('error', `O usuario ${id_usuario} Falhou em gerar um relatorio: ${err.message}`, 'Falha', 'Relatorio Status DM', id_cliente, id_usuario);
         response.status(500).send('Erro ao executar consulta');
     }
 }
