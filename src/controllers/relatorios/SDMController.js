25ab758f1bea263ccb109a7947c82220a4f2c2f8
Lab220Dev <suporte@lab220.com.br>
2024-10-29 17:37:04 -0300

    Ajustes
    Pequenos ajustes

--- src/controllers/relatorios/SDMController.js
+++ src/controllers/relatorios/SDMController.js
@@ -1,39 +1,41 @@
-const sql = require('mssql');
+const sql = require("mssql");
 
 async function relatorio(request, response) {
-    try {
-        const { id_cliente, id_dm, dia } = request.body;
+  try {
+    const { id_cliente, id_dm, dia } = request.body;
 
-        if (!id_cliente) {
-            return response.status(401).json("ID do cliente não enviado");
-        }
-
-        if (!id_dm) {
-            return response.status(401).json("ID da DM não enviado");
-        }
-
-        const currentDate = dia ? new Date(dia) : new Date();
-        const formattedDate = currentDate.toISOString().split('T')[0];
-
-        const query = `
-            SELECT * FROM DM_status 
-            WHERE ID_DM = @id_dm 
-              AND id_cliente = @id_cliente 
-              AND CONVERT(date, dataHora) = @dia
-        `;
-
-        const dbRequest = new sql.Request();
-        dbRequest.input('id_cliente', sql.Int, id_cliente);
-        dbRequest.input('id_dm', sql.Int, id_dm);
-        dbRequest.input('dia', sql.Date, formattedDate);
-
-        const result = await dbRequest.query(query);
-        response.status(200).json(result.recordset);
-    } catch (error) {
-        console.error('Erro ao executar consulta:', error.message);
-        response.status(500).send('Erro ao executar consulta');
+    if (!id_cliente) {
+      return response.status(401).json("ID do cliente não enviado");
     }
+
+    if (!id_dm) {
+      return response.status(401).json("ID da DM não enviado");
+    }
+
+    const currentDate = dia ? new Date(dia) : new Date();
+    const formattedDate = currentDate.toISOString().split("T")[0];
+
+    let query;
+    const dbRequest = new sql.Request();
+    dbRequest.input("id_cliente", sql.Int, id_cliente);
+    dbRequest.input("dia", sql.Date, formattedDate);
+
+    if (id_dm === null) {
+      query =
+        "SELECT * FROM DM_status WHERE  id_cliente = @id_cliente AND CONVERT(date, dataHora) = @dia";
+    } else {
+      query =
+        "SELECT * FROM DM_status WHERE ID_DM = @id_dm AND id_cliente = @id_cliente AND CONVERT(date, dataHora) = @dia";
+      dbRequest.input("ID_DM", sql.Int, id_dm);
+    }
+
+    const result = await dbRequest.query(query);
+    response.status(200).json(result.recordset);
+  } catch (error) {
+    console.error("Erro ao executar consulta:", error.message);
+    response.status(500).send("Erro ao executar consulta");
+  }
 }
 module.exports = {
-    relatorio
- };
+  relatorio,
+};
